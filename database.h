d2ee77a30f2393ed327cd40721939f78f0662057
clown <89036755470a@gmail.com>
2026-05-19 13:30:06 +0300

    feat: implement insert/select statement handling

--- database.h
+++ database.h
@@ -10,7 +10,28 @@ typedef struct {
     ssize_t input_length;
 } InputBuffer;
 
+typedef enum {
+    STATEMENT_INSERT,
+    STATEMENT_SELECT
+} StatementType;
 
+typedef struct {
+    StatementType type;
+} Statement;
+
+typedef enum {
+    META_COMMAND_SUCCESS,
+    META_COMMAND_UNRECOGNIZED_COMMAND
+} MetaCommandResult;
+
+typedef enum {
+    PREPARE_SUCCESS,
+    PREPARE_UNRECOGNIZED_STATEMENT
+} PrepareResult;
+
+PrepareResult prepare_statement(InputBuffer* input_buffer, Statement* statement);
+void execute_statement(Statement* statement);
+MetaCommandResult do_meta_command(InputBuffer* input_buffer);
 void print_prompt();
 ssize_t getline(char **lineptr, size_t *n, FILE *stream);
 void read_input(InputBuffer* input_buffer);
